c3ba7fc3624fb2b732da2bb8b3ddc62448e4158c
CV. SOLUSIKREATIF ANAK INDONESIA <solusikreatifanakindonesia@gmail.com>
2025-02-26 22:34:54 +0700

    add module to remove enterprise or unused modules

---
+++ addons/remove_enterprise/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2018 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# Copyright 2025 Sleektiv (www.sleektiv.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import models
---
+++ addons/remove_enterprise/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
+# Copyright 2025 Sleektiv (www.sleektiv.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Remove Enterprise",
+    "summary": "Remove enterprise modules and setting items",
+    "version": "18.0.1.0.0",
+    "category": "Maintenance",
+    "author": "Eska, Onestein, Odoo Community Association (OCA), Sleektiv",
+    "website": "https://sleektiv.com",
+    "license": "AGPL-3",
+    "depends": ["base_setup"],
+    "data": ["views/res_config_settings_views.xml"],
+    "installable": True,
+}
